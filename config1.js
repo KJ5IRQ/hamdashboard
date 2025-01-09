d4b682685285b942a554e416694c4ad00539ba6c
KJ5IRQ <kj5irq@gmail.com>
2025-01-09 16:41:09 -0600

    Create config1.js

---
+++ config1.js
@@ -0,0 +1,5 @@
+const topBarCenterStyle = "background: blue; text-align: center; font-weight: bold; color: yellow;";
+const callSign = "Your Call Sign Here";
+
+topBarCenter.style.cssText = topBarCenterStyle;
+topBarCenter.textContent = callSign;
